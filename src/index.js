64eb1acfb54ccc0d7903a9a606b7f1b4c6081d6a
Sean Ye <seanye24@gmail.com>
2020-07-14 02:28:00 -0700

    edit styling

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  // <React.StrictMode>
-  <App className='page' />,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App className='page' />, document.getElementById('root'));
