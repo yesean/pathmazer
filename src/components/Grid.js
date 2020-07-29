121c7a203e2c83f9ba71c90ffd89398f2c16940a
Sean Ye <seanye24@gmail.com>
2020-07-29 01:55:42 -0700

    separate event handlers from square

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Square from './Square.js';
 import Animations from './../services/Animations.js';
 import GridConstants from './../services/GridConstants.js';
@@ -20,58 +20,17 @@ const Grid = ({
   const [isHoldingStart, setIsHoldingStart] = useState(false);
   const [isHoldingEnd, setIsHoldingEnd] = useState(false);
   const [isWDown, setIsWDown] = useState(false);
+  const [mouseIsOver, setMouseIsOver] = useState(null);
 
-  const updateGridOnMouseDown = (sq) => {
-    const nextGrid = [...grid];
-    if (grid[sq] === GridConstants.START_SQ) {
-      setIsHoldingStart(true);
-    } else if (grid[sq] === GridConstants.END_SQ) {
-      setIsHoldingEnd(true);
-    } else if (grid[sq] === GridConstants.WEIGHT_SQ) {
-      if (isWDown) {
-        nextGrid[sq] = GridConstants.DEFAULT_SQ;
-      } else {
-        nextGrid[sq] = GridConstants.WALL_SQ;
-      }
-    } else if (grid[sq] === GridConstants.WALL_SQ) {
-      if (isWDown) {
-        nextGrid[sq] = GridConstants.WEIGHT_SQ;
-      } else {
-        nextGrid[sq] = GridConstants.DEFAULT_SQ;
-      }
-    } else {
-      if (isWDown) {
-        nextGrid[sq] = GridConstants.WEIGHT_SQ;
-      } else {
-        nextGrid[sq] = GridConstants.WALL_SQ;
-      }
-    }
-    setGrid(nextGrid);
-  };
-
-  const updateGridOnMouseEnter = (sq) => {
-    const startSq = grid.findIndex((s) => s === GridConstants.START_SQ);
-    const endSq = grid.findIndex((s) => s === GridConstants.END_SQ);
-    if (isMouseDown && (sq !== startSq) & (sq !== endSq)) {
-      const nextGrid = [...grid];
-      if (isHoldingStart) {
-        nextGrid[startSq] = startIsCovering;
-        nextGrid[sq] = GridConstants.START_SQ;
-        setStartIsCovering(grid[sq]);
-        if (isAnimatingFinished) {
-          Animations.animate(algorithm, nextGrid, setGrid, 'none');
-          return;
-        }
-      } else if (isHoldingEnd) {
-        nextGrid[endSq] = endIsCovering;
-        nextGrid[sq] = GridConstants.END_SQ;
-        setEndIsCovering(grid[sq]);
-        if (isAnimatingFinished) {
-          Animations.animate(algorithm, nextGrid, setGrid, 'none');
-          return;
-        }
-      } else {
-        if (grid[sq] === GridConstants.WEIGHT_SQ) {
+  useEffect(() => {
+    const updateGridOnMouseDown = (sq) => {
+      if (!isAnimating && isMouseDown) {
+        const nextGrid = [...grid];
+        if (grid[sq] === GridConstants.START_SQ) {
+          setIsHoldingStart(true);
+        } else if (grid[sq] === GridConstants.END_SQ) {
+          setIsHoldingEnd(true);
+        } else if (grid[sq] === GridConstants.WEIGHT_SQ) {
           if (isWDown) {
             nextGrid[sq] = GridConstants.DEFAULT_SQ;
           } else {
@@ -90,57 +49,93 @@ const Grid = ({
             nextGrid[sq] = GridConstants.WALL_SQ;
           }
         }
+        setGrid(nextGrid);
       }
-      setGrid(nextGrid);
-    }
-  };
+    };
+    updateGridOnMouseDown(mouseIsOver);
+  }, [isMouseDown, isAnimating]);
 
-  const onMouseEnter = (sq) => {
-    if (!isAnimating) {
-      updateGridOnMouseEnter(sq);
-    }
-  };
-
-  const onMouseDown = (sq) => {
-    if (!isAnimating) {
-      updateGridOnMouseDown(sq);
-      setIsMouseDown(true);
-    }
-  };
-
-  const onMouseUp = (sq) => {
-    if (!isAnimating) {
-      if (isHoldingStart) {
-        setIsHoldingStart(false);
-      } else if (isHoldingEnd) {
-        setIsHoldingEnd(false);
+  useEffect(() => {
+    const updateGridOnMouseEnter = (sq) => {
+      const startSq = grid.findIndex((s) => s === GridConstants.START_SQ);
+      const endSq = grid.findIndex((s) => s === GridConstants.END_SQ);
+      if (!isAnimating && isMouseDown && (sq !== startSq) & (sq !== endSq)) {
+        const nextGrid = [...grid];
+        if (isHoldingStart) {
+          nextGrid[startSq] = startIsCovering;
+          nextGrid[sq] = GridConstants.START_SQ;
+          setStartIsCovering(grid[sq]);
+          if (isAnimatingFinished) {
+            Animations.animate(algorithm, nextGrid, setGrid, 'none');
+            return;
+          }
+        } else if (isHoldingEnd) {
+          nextGrid[endSq] = endIsCovering;
+          nextGrid[sq] = GridConstants.END_SQ;
+          setEndIsCovering(grid[sq]);
+          if (isAnimatingFinished) {
+            Animations.animate(algorithm, nextGrid, setGrid, 'none');
+            return;
+          }
+        } else {
+          if (grid[sq] === GridConstants.WEIGHT_SQ) {
+            if (isWDown) {
+              nextGrid[sq] = GridConstants.DEFAULT_SQ;
+            } else {
+              nextGrid[sq] = GridConstants.WALL_SQ;
+            }
+          } else if (grid[sq] === GridConstants.WALL_SQ) {
+            if (isWDown) {
+              nextGrid[sq] = GridConstants.WEIGHT_SQ;
+            } else {
+              nextGrid[sq] = GridConstants.DEFAULT_SQ;
+            }
+          } else {
+            if (isWDown) {
+              nextGrid[sq] = GridConstants.WEIGHT_SQ;
+            } else {
+              nextGrid[sq] = GridConstants.WALL_SQ;
+            }
+          }
+        }
+        setGrid(nextGrid);
       }
-      setIsMouseDown(false);
-    }
-  };
+    };
+    updateGridOnMouseEnter(mouseIsOver);
+  }, [mouseIsOver, isAnimating]);
 
-  const onKeyDown = (e) => {
-    if (!isAnimating) {
-      if (e.key === 'w') {
-        setIsWDown(true);
-      }
-    }
-  };
+  const onMouseEnter = useCallback((sq) => {
+    setMouseIsOver(sq);
+  }, []);
 
-  const onKeyUp = (e) => {
-    if (!isAnimating) {
-      if (e.key === 'w') {
-        setIsWDown(false);
-      }
-    }
-  };
+  const onMouseDown = useCallback((sq) => {
+    setIsMouseDown(true);
+  }, []);
 
-  const renderSquare = (sqType, sq) => {
+  const onMouseUp = useCallback((sq) => {
+    setIsHoldingStart(false);
+    setIsHoldingEnd(false);
+    setIsMouseDown(false);
+  }, []);
+
+  const onKeyDown = useCallback((e) => {
+    if (e.key === 'w') {
+      setIsWDown(true);
+    }
+  }, []);
+
+  const onKeyUp = useCallback((e) => {
+    if (e.key === 'w') {
+      setIsWDown(false);
+    }
+  }, []);
+
+  const renderSquare = (sq) => {
     return (
       <Square
-        key={sq}
         id={sq}
-        className={sqType}
+        key={sq}
+        className={grid[sq]}
         onMouseEnter={() => onMouseEnter(sq)}
         onMouseDown={() => onMouseDown(sq)}
         onMouseUp={() => onMouseUp(sq)}
@@ -151,7 +146,7 @@ const Grid = ({
   };
 
   const renderGrid = () => {
-    return grid.map((sqType, sq) => renderSquare(sqType, sq));
+    return grid.map((sqType, sq) => renderSquare(sq));
   };
 
   return (
@@ -161,4 +156,4 @@ const Grid = ({
   );
 };
 
-export default Grid;
+export default React.memo(Grid);
