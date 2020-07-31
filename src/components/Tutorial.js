a16e46f6290ca9d3abbd392071793fe21973216b
Sean Ye <seanye24@gmail.com>
2020-07-30 19:44:43 -0700

    update Tutorial page

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -3,19 +3,20 @@ import './../styles/Tutorial.css';
 
 const Tutorial = () => {
   const [textIndex, setTextIndex] = useState(0);
-  const [style, setStyle] = useState({});
   const [prevButtonStyle, setPrevButtonStyle] = useState({
     backgroundColor: '#94caec',
   });
   const [nextButtonStyle, setNextButtonStyle] = useState({});
+  const [shouldShowTutorialPage, setShouldShowTutorialPage] = useState(true);
   const [shouldShowAlgorithms, setShouldShowAlgorithms] = useState(false);
+  const [shouldShowSiteOptions, setShouldShowSiteOptions] = useState(false);
 
   const texts = [
     [
       'Pathfinding, or the process of finding a route between two points, is an ' +
         'important topic in computer science and graph theory.',
       'Tasks, like solving mazes, navigating video game terrain, and calculating ' +
-        'Google Maps results, all utilize pathfinding techniques.',
+        'Google Maps, all utilize pathfinding techniques.',
     ],
     [
       'Over time, different algorithms have emerged in pathfinding. Some are ' +
@@ -24,13 +25,11 @@ const Tutorial = () => {
     ],
     [
       'This site allows you to visualize and play around with how these ' +
-        'algorithms work. You can move around the start and end square. You can also click and drag to add walls (impenetrable) ' +
-        'or hold W to add weights (penetrable, cost 10 squares) instead.',
-      'You can move around the start and end square after a visualization to see how the new position affects path finding.',
-      'Lastly, you can adjust the speed of visualization as well as generate mazes to see how these algorithms solve them!',
+        'algorithms work.',
     ],
   ];
   const algorithmsIndex = 1;
+  const siteOptionsIndex = 2;
 
   const algorithms = [
     ['Dijkstra:', 'Gurantees Shortest Path', 'Weighted', 'Slow'],
@@ -40,8 +39,20 @@ const Tutorial = () => {
     ['DFS:', "Doesn't Guarantee Shortest Path", 'Unweighted', 'Slow'],
   ];
 
+  const siteOptions = [
+    ['Move around the start and end square'],
+    ['Click and drag to add walls (impenetrable)'],
+    ['Hold W to add weights (penetrable, cost 10 squares) instead'],
+    [
+      'Move around the start and end square after a visualization to see how the new position affects path finding',
+    ],
+    [
+      'Adjust the speed of visualization as well as generate mazes to see how these algorithms solve them!',
+    ],
+  ];
+
   const onSkip = () => {
-    setStyle({ display: 'none' });
+    setShouldShowTutorialPage(false);
   };
   const onPrev = () => {
     if (textIndex > 0) {
@@ -76,43 +87,68 @@ const Tutorial = () => {
     } else {
       setShouldShowAlgorithms(false);
     }
+    if (textIndex === siteOptionsIndex) {
+      setShouldShowSiteOptions(true);
+    } else {
+      setShouldShowSiteOptions(false);
+    }
   };
 
   return (
-    <div className='tutorialPage' style={style}>
-      <div className='tutorialTextContainer'>
-        {texts[textIndex].map((text) => (
-          <p className='tutorialText'>{text}</p>
-        ))}
-        <div className='algorithmListContainer'>
-          <table className='algorithmList'>
-            <tbody>
-              {shouldShowAlgorithms &&
-                algorithms.map((alg) => (
-                  <tr>
-                    {alg.map((elem) => (
-                      <td>{elem}</td>
-                    ))}
-                  </tr>
+    shouldShowTutorialPage && (
+      <div className='tutorialPage'>
+        <div className='tutorialTextContainer'>
+          {texts[textIndex].map((text) => (
+            <p key={text} className='tutorialText'>
+              {text}
+            </p>
+          ))}
+          {shouldShowAlgorithms && (
+            <div className='algorithmListContainer'>
+              <table className='algorithmList'>
+                <tbody>
+                  {algorithms.map((alg) => (
+                    <tr key={alg}>
+                      {alg.map((elem) => (
+                        <td key={elem}>{elem}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+          {shouldShowSiteOptions && (
+            <div className='siteOptionsListContainer'>
+              <ul className='siteOptionsList'>
+                {siteOptions.map((option) => (
+                  <li key={option}>{option}</li>
                 ))}
-            </tbody>
-          </table>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className='directionContainer'>
+          <button
+            className='prevButton'
+            onClick={onPrev}
+            style={prevButtonStyle}>
+            {String.fromCharCode(8592)}
+          </button>
+          <button
+            className='nextButton'
+            onClick={onNext}
+            style={nextButtonStyle}>
+            {String.fromCharCode(8594)}
+          </button>
+        </div>
+        <div className='skipContainer'>
+          <button className='skipButton' onClick={onSkip}>
+            {String.fromCharCode(10005)}
+          </button>
         </div>
       </div>
-      <div className='directionContainer'>
-        <button className='prevButton' onClick={onPrev} style={prevButtonStyle}>
-          {String.fromCharCode(8592)}
-        </button>
-        <button className='nextButton' onClick={onNext} style={nextButtonStyle}>
-          {String.fromCharCode(8594)}
-        </button>
-      </div>
-      <div className='skipContainer'>
-        <button className='skipButton' onClick={onSkip}>
-          {String.fromCharCode(10005)}
-        </button>
-      </div>
-    </div>
+    )
   );
 };
 
