70d908dab101c3e3970c26247a074a1c8cbea887
Sean Ye <seanye24@gmail.com>
2020-08-04 22:14:19 -0700

    uibuild

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname + 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   console.log('user joined');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  console.log(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => { console.log('server listening on PORT', PORT); });
