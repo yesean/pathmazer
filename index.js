9f15578734d4231d1cf5e146f54cd2285bfe6d99
Sean Ye <seanye24@gmail.com>
2020-08-06 16:57:02 -0700

    add nodemon

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 
 app.get('/', (req, res) => {
   console.log('user joined');
