ba1d1a719e967d2fad27f854052737514785e9b4
Sean Ye <seanye24@gmail.com>
2021-08-29 00:23:25 -0700

    Update build

---
+++ build/precache-manifest.ee38da90c09715fcbb93d4647eb1e909.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9625138699fdf397b30fc7be77c7a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a77ddc9643f166e02dd9",
+    "url": "/static/css/main.497f7329.chunk.css"
+  },
+  {
+    "revision": "56d4bb2cf69ebf62ad7b",
+    "url": "/static/js/2.99cdd835.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.99cdd835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77ddc9643f166e02dd9",
+    "url": "/static/js/main.dfd514ca.chunk.js"
+  },
+  {
+    "revision": "05270871d403934f033d",
+    "url": "/static/js/runtime-main.3a3e0e65.js"
+  },
+  {
+    "revision": "6820b192218584e97de8f4be5296d287",
+    "url": "/static/media/algorithm.6820b192.svg"
+  },
+  {
+    "revision": "5a07179122714649778a8d285f11aad8",
+    "url": "/static/media/creativity.5a071791.svg"
+  },
+  {
+    "revision": "93326d55d62e75ed0c8eaa00bf2dadea",
+    "url": "/static/media/end.93326d55.svg"
+  },
+  {
+    "revision": "197c485d19b2f4638f6a9243a861a208",
+    "url": "/static/media/map.197c485d.svg"
+  },
+  {
+    "revision": "ff2b96719fb93f9698bfe4070dea4e27",
+    "url": "/static/media/start.ff2b9671.svg"
+  },
+  {
+    "revision": "17d008f4bb061c33e06c6dfca1388df8",
+    "url": "/static/media/weight.17d008f4.png"
+  },
+  {
+    "revision": "f8a5f7ddf8726aa97587a8f02e1a52d6",
+    "url": "/static/media/weight.f8a5f7dd.svg"
+  }
+]);
